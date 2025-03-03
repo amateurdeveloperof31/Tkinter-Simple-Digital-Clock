707d61786a37fc3271a7cd8130ff74b9c2303a8c
amateurdeveloperof31 <amateurdeveloperof31@gmail.com>
2025-03-03 18:26:10 +0530

    Update digital_clock.py

--- digital_clock.py
+++ digital_clock.py
@@ -3,26 +3,38 @@ from tkinter import *
 from datetime import datetime
 import pyglet
 
+# --------------------------------------------------Settings------------------------------------------------------------
+width =200  # Window Width
+height = 50  # Window Height
+font = 'digital-7.ttf'  # Custom Font File
+main_background = 'white'  # Main Background Color
+hour_color = '#fa0a2f'
+minute_color = '#0032ff'
+seconds_color = '#FF5733'
+ampm_color = '#cc03bd'
+date_color = '#333333'
+
 # --------------------------------------------------Main Class----------------------------------------------------------
 class DigitalClock:
     def __init__(self):
         self.windows = Tk()
         self.windows.title("Digital Clock")
-        width, height = 200, 50
         self.windows.minsize(width, height)
         self.windows.resizable(False, False)
         self.windows.config(bg="white")
 
         pyglet.options['win32_gdi_font'] = True
-        pyglet.font.add_file('digital-7.ttf')
+        pyglet.font.add_file(font)
 
-        self.canvas = Canvas(width=200, height=50, bg="white", highlightthickness=0)
-        self.hour_text = self.canvas.create_text(40, 20, text="00", font=('digital-7', 16), fill='#fa0a2f')
-        self.minute_text = self.canvas.create_text(80, 20, text="00", font=('digital-7', 16), fill='#0032ff')
-        self.seconds_text = self.canvas.create_text(120, 20, text="00", font=('digital-7', 16), fill='#FF5733')
-        self.ampm_text = self.canvas.create_text(160, 20, text="AM", font=('digital-7', 16), fill='#cc03bd')
-        self.date_text = self.canvas.create_text(100, 40, text="Monday 01-01-2024", font=('digital-7', 10), fill='#333333')
+        self.canvas = Canvas(self.windows, width=200, height=50, bg=main_background, highlightthickness=0)
         self.canvas.pack()
+
+        self.hour_text = self.canvas.create_text(40, 20, text="00", font=('digital-7', 16), fill=hour_color)
+        self.minute_text = self.canvas.create_text(80, 20, text="00", font=('digital-7', 16), fill=minute_color)
+        self.seconds_text = self.canvas.create_text(120, 20, text="00", font=('digital-7', 16), fill=seconds_color)
+        self.ampm_text = self.canvas.create_text(160, 20, text="AM", font=('digital-7', 16), fill=ampm_color)
+        self.date_text = self.canvas.create_text(100, 40, text="Monday 01-01-2024", font=('digital-7', 10),
+                                                 fill=date_color)
 
         self.update_clock()
 
@@ -30,21 +42,15 @@ class DigitalClock:
 
 # -------------------------------------------------Update Clock---------------------------------------------------------
     def update_clock(self):
-        hour_now = datetime.now().strftime("%I")
-        minute_now = datetime.now().strftime("%M")
-        seconds_now = datetime.now().strftime("%S")
-        ampm_now = datetime.now().strftime("%p")
+        time_now = datetime.now()
 
-        date_now = datetime.now().strftime("%A, %d-%B-%Y")
+        self.canvas.itemconfig(self.hour_text, text=time_now.strftime("%I"))
+        self.canvas.itemconfig(self.minute_text, text=time_now.strftime("%M"))
+        self.canvas.itemconfig(self.seconds_text, text=time_now.strftime("%S"))
+        self.canvas.itemconfig(self.ampm_text, text=time_now.strftime("%p"))
+        self.canvas.itemconfig(self.date_text, text=time_now.strftime("%A, %d-%B-%Y"))
 
-        self.canvas.itemconfig(self.hour_text, text=hour_now)
-        self.canvas.itemconfig(self.minute_text, text=minute_now)
-        self.canvas.itemconfig(self.seconds_text, text=seconds_now)
-        self.canvas.itemconfig(self.ampm_text, text=ampm_now)
-
-        self.canvas.itemconfig(self.date_text, text=date_now)
-
-        self.windows.after(1000, lambda: self.update_clock())
+        self.windows.after(1000, self.update_clock)
 
 # -----------------------------------------------Run the Program--------------------------------------------------------
 DigitalClock()
